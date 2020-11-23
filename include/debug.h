5e5dc361f735fab3c8de37f79de44a8eb81f402f
Brad Whitted <whittedb@the-zoo.net>
2020-11-23 00:46:57 -0600

    - Update Queue to ArduinoQueue

--- include/debug.h
+++ include/debug.h
@@ -1,19 +1,13 @@
 #pragma once
-#include <string>
 
 #ifdef RTT_DEBUG
     #include "SEGGER_RTT.h"
     #define DEBUG_BEGIN
     #define DEBUG_WAIT_FOR_INIT
 
-    void DEBUG_PRINT(const char *msg, unsigned id=0) {SEGGER_RTT_WriteString(id, msg);}
-    void DEBUG_PRINTLN(const char *msg, unsigned id=0) {SEGGER_RTT_WriteString(id, std::string(msg).append("\r\n").c_str());}
-    static void DEBUG_PRINTF(unsigned id, const char *fmt, ...) {
-        va_list args;
-        va_start(args, fmt);
-        SEGGER_RTT_printf(id, fmt, args);
-        va_end(args);
-    }
+    void DEBUG_PRINT(const char *msg, unsigned id=0);
+    void DEBUG_PRINTLN(const char *msg, unsigned id=0);
+    void DEBUG_PRINTF(unsigned id, const char *fmt, ...);
 #else
     #ifdef MY_DEBUG
         #ifndef DEBUG_SERIAL_SPEED
@@ -21,14 +15,12 @@
         #endif
         #define DEBUG_BEGIN Serial.begin(DEBUG_SERIAL_SPEED)
         #define DEBUG_WAIT_FOR_INIT while(!Serial){}
-        #define DEBUG_PRINT(msg, id) {Serial.print(F(msg));}
-        #define DEBUG_PRINTLN(msg,id) {Serial.println(F(msg));}
-        static void DEBUG_PRINTF(unsigned id, const char *fmt, ...) {
-            va_list args;
-            va_start(args, fmt);
-            Serial.printf(fmt, args);
-            va_end(args);
-        }
+        void DEBUG_PRINT(const char *msg, unsigned id=0);
+        void DEBUG_PRINT(const __FlashStringHelper *msg, unsigned id=0);
+        void DEBUG_PRINTLN(const char *msg, unsigned id=0);
+        void DEBUG_PRINTLN(const __FlashStringHelper *msg, unsigned id=0);
+        void DEBUG_PRINTF(unsigned id, const char *fmt, ...);
+        void DEBUG_PRINTF(unsigned id, const __FlashStringHelper *fmt...);
     #else
         #define DEBUG_BEGIN
         #define DEBUG_WAIT_FOR_INIT
--- src/debug.cpp
+++ src/debug.cpp
@@ -1,12 +1,50 @@
 #include "debug.h"
 
-
 #ifdef __arm__
 // should use uinstd.h to define sbrk but Due causes a conflict
 extern "C" char* sbrk(int incr);
 #else  // __ARM__
 extern char *__brkval;
 #endif  // __arm__
+
+#ifdef RTT_DEBUG
+  #include <SEGGER_RTT.h>
+  void DEBUG_PRINT(const char *msg, unsigned id) {SEGGER_RTT_printf(id, msg);}
+  void DEBUG_PRINTLN(const char *msg, unsigned id) {SEGGER_RTT_printf(id, msg); SEGGER_RTT_printf(id, "\r\n");}
+  void DEBUG_PRINTF(unsigned id, const char *fmt, ...) {
+      va_list args;
+      va_start(args, fmt);
+      SEGGER_RTT_printf(id, fmt, args);
+      va_end(args);
+  }
+#else
+    #ifdef MY_DEBUG
+        void DEBUG_PRINT(const char *msg, unsigned id) {Serial.print(msg);}
+        void DEBUG_PRINT(const __FlashStringHelper *msg, unsigned id) {Serial.print(msg);}
+        void DEBUG_PRINTLN(const char *msg, unsigned id) {Serial.print(msg); Serial.print("\r\n");}
+        void DEBUG_PRINTLN(const __FlashStringHelper *msg, unsigned id) {
+          Serial.print(msg);
+          Serial.print("\r\n");
+        }
+        void DEBUG_PRINTF(unsigned id, const char *fmt, ...) {
+            char b[128];
+            va_list args;
+            va_start(args, fmt);
+            sprintf(b, fmt, args);
+            Serial.print(b);
+            va_end(args);
+        }
+
+        void DEBUG_PRINTF(unsigned id, const __FlashStringHelper *fmt...) {
+            char b[128];
+            va_list args;
+            va_start(args, fmt);
+            sprintf(b, String(fmt).c_str(), args);
+            Serial.print(b);
+            va_end(args);
+        }
+    #endif
+#endif
 
 int freeMemory() {
   char top;
